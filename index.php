a07c3d3f6fccf1eef97cba9a8386fb284376f81e
DVortelinas <jvortelinas@gmail.com>
2024-04-13 15:15:04 +0200

    last

--- index.php
+++ index.php
@@ -101,79 +101,73 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
   
 <!-- heading -->
-<nav class="navbar bg-muted">
-  <div class="container-fluid">
+<!-- <nav class="navbar bg-muted">
+  <div class="container">
     <span class=" navbar-brand mb-0"> <?php echo date("l jS \of F Y h:i:s A")?></span>
+    
   </div>
-</nav>
+</nav> -->
 <!--end heading -->
 
-
-<div class="container mt-5">
-<div class="row g-0  pt-5">
-    <!-- ------left--------- -->
-  <!-- <div class="col-sm-6 col-md-7"> 
-  <h1>MyPortal</h1> 
-     <img src="img/photo3.jpg" class="img-fluid" alt="...">
-    
-     <p>Lorem ipsum dolor sit, amet consectetur adipisicing
-    Harum obcaecati, quo fugiat suscipit 
-    voluptate deleniti consectetur odit doloribus neque hic, odio praesentium eum debitis et mollitia!</p>
-  </div> -->
-
-<!-- -----------right------------- -->
-
-  <div class="col-sm-6 col-md-5">
-
-  <div class="ms-sm-5">
-    
-        <h1>Login</h1>
-        <div class="infos">
-            <p>*If you don't have already acount please contact with ITgroup</p>
-        <p>Please fill in your credentials to login.</p>
-        </div>
-        
-
-        <?php 
-        if(!empty($login_err)){
-            echo '<div class="alert alert-danger">' . $login_err . '</div>';
-        }        
-        ?>
+<div class="container front ">
+<h1 class="mt-5 display-1">MyPortal</h1> 
+       
+       <h6 class="mt-2">Lorem ipsum dolor sit, amet consectetur adipisicing
+      Harum obcaecati, quo fugiat suscipit 
+      voluptate deleniti consectetur odit doloribus neque hic, odio praesentium eum debitis et mollitia!</h6>
+  <!-- --------------------modal------------------------- -->
   
-
-
+  <h6>*If you don't have acount please contact with ITgroup</h6>
+  <button type="button" class="btn btn-primary mt-4 " data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Login</button>
+  
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <p class="modal-title fs-5" id="exampleModalLabel">Please fill in your credentials to login</p>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+        <?php 
+          if(!empty($login_err)){
+              echo '<div class="alert alert-danger">' . $login_err . '</div>';
+          }        
+          ?>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <div class="mb-3">
-                <label  class="form-label">Username</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="mb-3">
-                <label  class="form-label">Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group mb-5">
-                <input type="submit" class="btn btn-primary" value="Login">
-            </div>
-            <p>Forgot your password? <a href="login_system/reset-password.php">reset your password now</a>.</p>
-            <p>Don't have an account? <a href="login_system/register.php">Sign up now</a>.</p>
-        </form>
+          <div class="mb-3">
+                  <label  class="form-label">Username</label>
+                  <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                  <span class="invalid-feedback"><?php echo $username_err; ?></span>
+              </div>    
+              <div class="mb-3">
+                  <label  class="form-label">Password</label>
+                  <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                  <span class="invalid-feedback"><?php echo $password_err; ?></span>
+              </div>
+              <div class="form-group mb-5">
+                  <input type="submit" class="btn btn-primary" value="Login">
+              </div>
+              <p>Forgot your password? <a href="login_system/reset-password.php">reset your password now</a>.</p>
+              <p>Don't have an account? <a href="login_system/register.php">Sign up now</a>.</p>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Send message</button>
+        </div>
+      </div>
     </div>
-
-
   </div>
-</div>
 
 </div>
 
 
 
-<!-- <footer class="footer">
+<footer class="footer">
   <h6 class="text-center  fs-6 bg-dark text-light">D.Vortelinas Copyright<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-c-circle" viewBox="0 0 16 16">
   <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.146 4.992c-1.212 0-1.927.92-1.927 2.502v1.06c0 1.571.703 2.462 1.927 2.462.979 0 1.641-.586 1.729-1.418h1.295v.093c-.1 1.448-1.354 2.467-3.03 2.467-2.091 0-3.269-1.336-3.269-3.603V7.482c0-2.261 1.201-3.638 3.27-3.638 1.681 0 2.935 1.054 3.029 2.572v.088H9.875c-.088-.879-.768-1.512-1.729-1.512"/>
 </svg> <?php echo date("Y");?></h6>
-</footer> -->
+</footer>
 
 
 
